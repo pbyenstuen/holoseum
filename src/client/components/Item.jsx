13dbfeac1d4d1b7e0b711702359598689c3bb095
Magnus Oma <magnus.oma@gmail.com>
2021-05-19 16:09:12 +0200

    Istallerte axios og satt opp call mot api'et. Har problemer med at ting rendrer før api'et svarer

--- src/client/components/Item.jsx
+++ src/client/components/Item.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
 import { useParams } from "react-router";
-import useLoader from "./useLoader";
+//import useLoader from "./useLoader";
+import axios from 'axios';
+//import { response } from 'express';
 
 const Item = ({ api }) => {
     const { name } = useParams();
-    const { data: item, loading, error, reload } = useLoader(async () => await api.getItem(name), [name]);
+    console.log(name);
+    let hollowgramObj;
 
-    if (loading) {
+    
+
+    //const { data: item, loading, error, reload } = useLoader(async () => await api.getItem(name), [name]);
+
+    /*if (loading) {
         return <h2>Loading item...</h2>
-    }
+    }*/
+
+    //Henter hollogramObj fra webapi og consol logger
+    axios.get(`https://localhost:5001/objecthollowgram/${name}`)
+        .then( res => {
+            hollowgramObj = res.data;
+            console.log(hollowgramObj);
+        })
+    
 
     return (
         <div>
-            <h1>{item.name}</h1>
-            <h2>url: {item.url}</h2>
+            <h1>{name}</h1>
+            <h2>url: </h2>
         </div>
     )
 }
+
 
 export default Item;
