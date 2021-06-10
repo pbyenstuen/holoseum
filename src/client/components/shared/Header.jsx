4098420800c5c29ef304cd524b8876f04dacaa65
pbyenstuen <56080700+pbyenstuen@users.noreply.github.com>
2021-06-10 15:35:51 +0200

    Fix mobile admin issues

--- src/client/components/shared/Header.jsx
+++ src/client/components/shared/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useHistory } from "react-router-dom";
+import useMediaQuery from "../../hooks/useMediaQuery";
 import useSubmit from "../../hooks/useSubmit";
 
 const Header = ({ api, user, updateUser }) => {
+    const mobile = useMediaQuery("(max-width: 768px)");
     const history = useHistory();
 
     const { handleSubmit: handleLogout, submitting } = useSubmit(
@@ -17,25 +19,34 @@ const Header = ({ api, user, updateUser }) => {
     );
 
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink exact={true} activeClassName="is-active" to='/'>HVA ER?</NavLink>
-                    </li>
-                    <li>
-                        <NavLink activeClassName="is-active" to='/kontakt'>KONTAKT</NavLink>
-                    </li>
-                    {
-                        user &&
+        <>
+            <header>
+                <nav>
+                    <ul>
                         <li>
-                            <NavLink activeClassName="is-active" to='/admin'>ADMIN</NavLink>
+                            <NavLink exact={true} activeClassName="is-active" to='/'>HVA ER?</NavLink>
                         </li>
-                    }
-                </ul>
-            </nav>
-            {user && <button disabled={submitting} onClick={handleLogout}>LOGG UT</button>}
-        </header>
+                        <li>
+                            <NavLink activeClassName="is-active" to='/kontakt'>KONTAKT</NavLink>
+                        </li>
+                        {
+                            user &&
+                            <li>
+                                {!mobile ?
+                                    <NavLink activeClassName="is-active" to='/admin'>ADMIN</NavLink>
+                                    :
+                                    <NavLink id="mobile-admin-link" activeClassName="is-active" to='/admin'>
+                                        <i className="fa fa-cog" title="Admin" aria-label="Admin"></i>
+                                    </NavLink>
+                                }
+                            </li>
+                        }
+                    </ul>
+                </nav>
+                {(!mobile && user) && <button disabled={submitting} onClick={handleLogout}>LOGG UT</button>}
+            </header>
+            {(mobile && user) && <button id="logout-btn" disabled={submitting} onClick={handleLogout}>LOGG UT</button>}
+        </>
     )
 }
 
